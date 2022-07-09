8211208a14123dce09c70b7a46fdb83897251053
calebczh <99517153+calebczh@users.noreply.github.com>
2022-07-10 05:09:52 +0800

    edited

--- lifehack/models.py
+++ lifehack/models.py
@@ -1,6 +1,23 @@
+from random import randint
 from django.db import models
+import json
 
 
 class User(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
     points = models.IntegerField(default=0)
+
+class Bin(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+    current = models.FloatField(default=0)
+
+
+f = open('busstop.json')
+# Read the JSON
+titles = json.load(f)
+# Create a Django model object for each object in the JSON 
+for title in titles:
+    b = Bin(name=title['Description'], latitude = title['Latitude'], longitude = title['Longitude'], current = randint(0, 100))
+    b.save()
--- lifehack/views.py
+++ lifehack/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Bin
+from .serializers import UserSerializer, BinSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -44,3 +44,37 @@ def user_info(request):
             return Response(serializer.data)
         return Response(serializer.errors,status =status.HTTP_404_BAD_REQUEST)
     
+
+@api_view(['GET', 'POST', 'PUT'])
+def bin_info(request):
+
+    if request.method == 'GET':
+        try:
+            bin = Bin.objects.get(pk=request.data["name"])
+        except Bin.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = BinSerializer(bin)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        try:
+            bin = Bin.objects.get(pk=request.data["name"])
+        except Bin.DoesNotExist:
+            serializer = BinSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    elif request.method == 'PUT':
+        try:
+            bin = Bin.objects.get(pk=request.data["name"])
+        except Bin.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = BinSerializer(bin, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status =status.HTTP_404_BAD_REQUEST)
